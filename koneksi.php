3aad0777fd8c83a799635af1c16a3dd4f8ecb123
Rafi Afriza <139036669+rafcnzo@users.noreply.github.com>
2024-12-03 11:24:31 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-$servername = getenv('mysql.railway.internal');
+$host = getenv('junction.proxy.rlwy.net');
 $username = getenv('root');
 $password = getenv('lGFtEBBSbSiCTyzaSossuciQsJtECvMt');
 $dbname = getenv('railway');
-$port = getenv(3306);
+$port = getenv(32607);
 
 // Membuat koneksi
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($host, $username, $password, $dbname, $port);
 
 // Cek koneksi
 if ($conn->connect_error) {
