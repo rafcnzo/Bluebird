53f5c7d7f89c066bce499fc3224c546e7d452fb9
Rafi Afriza <139036669+rafcnzo@users.noreply.github.com>
2024-12-04 13:11:13 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -13,6 +13,4 @@ $conn = new mysqli($host, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Connection successful!";
 ?>
