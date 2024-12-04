073d7005891a53928a3f0817be0168bfa928c117
Rafi Afriza <139036669+rafcnzo@users.noreply.github.com>
2024-12-04 13:09:14 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,12 @@
 <?php
-// Fetching environment variables
-$host = getenv('DB_HOST'); 
-$username = getenv('DB_USER');
-$password = getenv('DB_PASS');
-$dbname = getenv('DB_NAME');
-$port = getenv('DB_PORT');
+// Fetching environment variables provided by Railway
+$host = getenv('MYSQLHOST');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$dbname = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-// Debugging (Only use in development)
-// var_dump($host, $username, $password, $dbname, $port);
-
-// Creating connection
+// Creating a connection
 $conn = new mysqli($host, $username, $password, $dbname, $port);
 
 // Check connection
